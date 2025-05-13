4a981f0521b78b2154fafbee59eced6ade7519cf
Vitor Alexandre <vca2205@gmail.com>
2025-05-13 15:07:57 -0300

    Adicionando o html na main

---
+++ src/JS/script.js
@@ -0,0 +1,26 @@
+//DECLARANDO UM ARRAY DE IMAGENS
+
+let imagens = [
+    'src/assets/',
+    'src/assets/',
+    'src/assets/',
+];
+
+//DECLARANDO AS VARIÁVEIS
+
+let index = 0;
+let tempo = 3000;
+
+//CRIANDO A FUNÇÃO DO SLIDESHOW
+
+function slideShow() {
+    document.getElementById('image').src = imagens[index];
+    index++;
+    //VERIFICAR QUANTIDADE DE IMAGENS
+    if (index == imagens.length) {
+        index = 0;
+    }
+    setTimeout("slideShow()",tempo)
+}
+//EXECUTA A FUNÇÃO
+slideShow();
